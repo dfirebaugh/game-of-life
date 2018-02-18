9e95f92bafde398c5bf7780927995aa3d0cde9d3
dfirebaugh <dafirebaugh@gmail.com>
2018-02-18 02:23:41 -0500

    removing unnecessary state from Generateion.js

--- src/Components/Generation.js
+++ src/Components/Generation.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 class Generation extends React.Component{
-  constructor(){
-    super()
-    this.state={generation:0}
-  }
   render(){
     return (
       <h4> Generation: {this.props.gen}</h4>
